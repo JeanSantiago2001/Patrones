e7127637727e524616db2415c5506281c2efb207
Jean Santiago Tumialan <83663039+JeanSantiago2001@users.noreply.github.com>
2023-07-13 22:55:45 -0500

    Delete PatronesCreacionales directory

--- PatronesCreacionales/FactoryMethod/CreadorInforme.java
+++
@@ -1,9 +0,0 @@
-package FactoryMethod;
-
-// Clase concreta para los creadores de informes
-public class CreadorInforme extends CreadorDocumento {
-    // Sobrescribe el método abstracto para devolver un informe
-    public Documento crearDocumento() {
-        return new Informe();
-    }
-}
--- PatronesCreacionales/FactoryMethod/Factura.java
+++
@@ -1,9 +0,0 @@
-package FactoryMethod;
-
-// Clase concreta para las facturas
-public class Factura implements Documento {
-    public void imprimir() {
-        System.out.println("Imprimiendo factura...");
-    }
-}
-
